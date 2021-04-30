15f358e286818b1553bbdb1a914aecadb1660d30
Tom Black <tom.black06007@gmail.com>
2021-04-29 22:48:28 -0700

    Added some place-holder data.

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Dr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Zeus'},
+    {id: 14, name: 'Achilles'},
+    {id: 15, name: 'Super Man'},
+    {id: 16, name: 'Iron Man'},
+    {id: 17, name: 'The Hulk'},
+    {id: 18, name: 'The Scarlet Witch'},
+    {id: 19, name: 'Wolverine'},
+]
