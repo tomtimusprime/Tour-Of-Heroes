a047818d613337c9b58a72a69fe5eea495708ea7
Tom Black <tom.black06007@gmail.com>
2021-05-07 21:47:43 -0700

    Made some adjustments to the heroes.component.ts file.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,10 +14,16 @@ export class HeroesComponent implements OnInit {
   // }
 
   heroes = HEROES;
+  selectedHero? : Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+  }
+
+  onSelect(hero: Hero): void {
+      this.selectedHero = hero;
   }
 
 }
